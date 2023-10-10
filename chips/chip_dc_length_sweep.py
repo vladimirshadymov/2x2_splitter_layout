5e403d7d9003174716818f46570a7dbe385492d2
Vladimir Shadymov <vladimir.shadymov@gmail.com>
2023-10-10 17:31:01 +0300

    Added direction coupler sweep

--- chips/chip_dc_length_sweep.py
+++ chips/chip_dc_length_sweep.py
@@ -1,4 +1,5 @@
 import gdsfactory as gf
+import numpy as np
 
 chip = gf.Component()
 
@@ -18,12 +19,33 @@ EDGE_COUPLER_LENGTH = 150
 EDGE_COUPLER_HANGOUT = 100
 edge_coupler_si = gf.components.edge_coupler_silicon(length=EDGE_COUPLER_LENGTH, width1=0.5, width2=0.2, with_bbox=True, with_two_ports=True, cross_section='xs_sc', port_order_name=['o1', 'o2'], port_order_types=['optical', 'optical'], add_pins=True)
 edge_coupler_si = gf.components.extend_ports(edge_coupler_si, port_names=['o2'], length=EDGE_COUPLER_HANGOUT, port_type='optical', centered=False)
-left_edge_couplers = gf.components.edge_coupler_array(edge_coupler=edge_coupler_si, n=38, pitch=127.0, x_reflection=False, text_offset=[10, 20], text_rotation=0)
-left = chip.add_ref(left_edge_couplers)
-left.move([2500-EDGE_COUPLER_LENGTH-EDGE_COUPLER_HANGOUT/2, -127*38/2])
-
-right_edge_couplers = gf.components.edge_coupler_array(edge_coupler=edge_coupler_si, n=38, pitch=127.0, x_reflection=True, text_offset=[-130, 20], text_rotation=0)
+right_edge_couplers = gf.components.edge_coupler_array(edge_coupler=edge_coupler_si, n=38, pitch=127.0, x_reflection=False, text_offset=[10, 20], text_rotation=0)
 right = chip.add_ref(right_edge_couplers)
-right.move([-2500+EDGE_COUPLER_LENGTH+EDGE_COUPLER_HANGOUT/2, -127*38/2])
+right.move([2500-EDGE_COUPLER_LENGTH-EDGE_COUPLER_HANGOUT/2, -127*38/2+90])
+
+left_edge_couplers = gf.components.edge_coupler_array(edge_coupler=edge_coupler_si, n=38, pitch=127.0, x_reflection=True, text_offset=[-130, 20], text_rotation=0)
+left = chip.add_ref(left_edge_couplers)
+left.move([-2500+EDGE_COUPLER_LENGTH+EDGE_COUPLER_HANGOUT/2, -127*38/2+90])
+
+
+directional_coupler_lengths = np.linspace(10, 200, 19)
+COUPLING_GAP = 0.3
+
+for (idx, len) in enumerate(directional_coupler_lengths):
+    splitter = gf.components.coupler(gap=COUPLING_GAP, length=len, dy=4.0, dx=10.0, cross_section='xs_sc')
+    splitter_ref_tmp = chip.add_ref(splitter)
+    splitter_ref_tmp.move([0, -127*38/2 + 127/2 + 90 + 127*2*idx])
+
+    route = gf.routing.get_route(left.ports[f'o{76-idx*2}'], splitter_ref_tmp.ports['o1'])    
+    chip.add(route.references) 
+
+    route = gf.routing.get_route(left.ports[f'o{76-idx*2-1}'], splitter_ref_tmp.ports['o2'])    
+    chip.add(route.references) 
+
+    route = gf.routing.get_route(right.ports[f'o{idx*2+1}'], splitter_ref_tmp.ports['o4'])    
+    chip.add(route.references) 
+
+    route = gf.routing.get_route(right.ports[f'o{idx*2+2}'], splitter_ref_tmp.ports['o3'])    
+    chip.add(route.references) 
 
 chip.write_gds('chip.gds')
